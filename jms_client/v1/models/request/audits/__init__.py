f9037d668a75c81e903d7f42f9801a5f20c3f34b
jiangweidong <1053570670@qq.com>
2025-02-18 11:35:55 +0800

    feat: 支持用户改密日志的查询、删除、新建和修改

--- jms_client/v1/models/request/audits/__init__.py
+++ jms_client/v1/models/request/audits/__init__.py
@@ -1,3 +1,4 @@
+from .change_password_logs import *
 from .login_logs import *
 from .operate_logs import *
 from .user_sessions import *
--- tests/test_audits.py
+++ tests/test_audits.py
@@ -7,10 +7,12 @@ from jms_client.v1.client import Client
 from jms_client.v1.models.request.audits import (
     DescribeUserSessionsRequest, DetailUserSessionRequest,
     DescribeLoginLogsRequest, DetailLoginLogRequest,
-    DescribeOperateLogsRequest, DetailOperateLogRequest
+    DescribeOperateLogsRequest, DetailOperateLogRequest,
+    DescribeChangePasswordLogsRequest, DetailChangePasswordLogRequest,
 )
 from jms_client.v1.models.instance.audits import (
     UserSessionInstance, LoginLogInstance, OperateLogInstance,
+    ChangePasswordLogInstance,
 )
 from jms_client.v1.models.response import Response
 
@@ -89,6 +91,28 @@ class TestFunctionality(unittest.TestCase):
 
         self.assertTrue(resp.is_success())
         self.assertIsInstance(resp.get_data(), OperateLogInstance)
+
+    # --------------------- 改密日志 ---------------------
+    def test_list_change_password_logs(self):
+        """ 测试获取用户改密日志列表 """
+        self.client.set_org('7de34b6e-3319-49c2-ad8a-f8c3e4c470d2')
+        request = DescribeChangePasswordLogsRequest(
+            limit=2, date_from='2024-06-01 00:00:00',
+            date_to='2024-12-31 00:00:00'
+        )
+        resp: Response = self.client.do(request, with_model=True)
+
+        self.assertTrue(resp.is_success())
+        self.assertIsInstance(resp.get_data(), list)
+
+    def test_retrieve_change_password_logs(self):
+        """ 测试获取指定 ID 用户改密日志详情 """
+        self.client.set_org('7de34b6e-3319-49c2-ad8a-f8c3e4c470d2')
+        request = DetailChangePasswordLogRequest(id_='7e50357d-dea7-4ca6-95d6-615b38343c90')
+        resp: Response = self.client.do(request, with_model=True)
+
+        self.assertTrue(resp.is_success())
+        self.assertIsInstance(resp.get_data(), ChangePasswordLogInstance)
 
 
 if __name__ == '__main__':
